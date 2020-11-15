615e574132100f1f8728aac4958352981bc343a8
milanzagorac <zagoracm@gmail.com>
2020-11-15 12:22:53 -0500

    final submission

--- connect4.js
+++ connect4.js
@@ -1,155 +1,174 @@
-//connect 4 game
-const WIDTH = 7;
-const HEIGHT = 6;
+// selectors:
 
-let gameBoard = document.getElementById('board');
-let currentColumn;
-let currentRow;
-let currentPlayer;
-let id = 1;
-let winner = null;
-let isGameOver = false;
-let makeArray = makeBoard(HEIGHT);
+let tableRow = document.getElementsByTagName('tr');
+let tableCell = document.getElementsByTagName('td');
+let tableSlot = document.querySelectorAll('.slot');
+const playerTurn = document.querySelector('.player-turn');
+const resetBtn = document.querySelector('.reset');
+let playerOne;
+let playerTwo;
+let playerOneColor = 'red';
+let playerTwoColor = 'yellow';
 
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
+//loop over the possible cell locations
+for (let i = 0; i < tableCell.length; i++) {
+    tableCell[i].addEventListener('click', (ev) => {
+        //log coordinates of the cells that would be clicked
+        console.log(`${ev.target.parentElement.rowIndex}, ${ev.target.cellIndex}`)
+    });
+};
 
-function makeBoard(HEIGHT) {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-    let boardArr = [];
-    for (i = 0; i < HEIGHT; i++) {
-        let rowArr = [];
-        for (let j = 0; j < WIDTH; j++) {
-            rowArr.push(null);
+// player selection - type names when you start the game
+while (!playerOne) {
+    playerOne = prompt('Player One Enter your name. You are Red');
+}
+
+
+while (!playerTwo) {
+    playerTwo = prompt('Player Two Enter your name. You are Yellow');
+}
+
+playerTurn.textContent = `${playerOne}'s Turn!`
+
+//go through all of the cells on the board - make sure JS recognizes they aren't filled in
+// and add an event listener on each cell to trigger color changing function
+
+Array.prototype.forEach.call(tableCell, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = 'white'
+
+});
+
+let currentPlayer = playerOne;
+
+function changeColor(e) {
+    
+    let column = e.target.cellIndex;
+    let row = [];
+    //for loop to check cell availability - start at the bottom row which is index 5
+    for (let i = 5; i > -1; i--) {
+        if (tableRow[i].children[column].style.backgroundColor === 'white') {
+            row.push(tableRow[i].children[column]);
+            //changes to the specific player's color once the loop confirms that there is an open space
+
+            if (currentPlayer === playerOne) {
+                row[0].style.backgroundColor = playerOneColor;
+                if (horizontalCheck() || verticalCheck() || diagonalCheckOne() || diagonalCheckTwo()) {
+                    playerTurn.textContent = `${playerOne} Wins!`;
+                    playerTurn.style.color = playerOneColor;
+                    return (alert(`${currentPlayer} Wins!`));
+                } else if (drawCheck()) {
+                    playerTurn.textContent = 'Game is a draw';
+                    return alert('DRAW!')
+                } else {
+                    playerTurn.textContent = `${playerTwo}'s Turn!`;
+                    return currentPlayer = playerTwo;
+                    }
+            } else {
+                row[0].style.backgroundColor = playerTwoColor;
+                if (horizontalCheck() || verticalCheck() || diagonalCheckOne() || diagonalCheckTwo()) {
+                    playerTurn.textContent = `${playerTwo} Wins!`;
+                    playerTurn.style.color = playerTwoColor;
+                    return (alert(`${currentPlayer} Wins!`));
+                } else if (drawCheck()){
+                    playerTurn.textContent = 'Game is a draw';
+                    return alert('DRAW!');
+                } else {
+                    playerTurn.textContent = `${playerOne}'s Turn!`;
+                    return currentPlayer = playerOne;
+                }
+            }
         }
-        boardArr.push(rowArr);
     }
-    return boardArr;  
 };
 
 
-// make an HTML board inside of the table inside of the main div container.
+//win checks
+// compare a function which compares 4 different slots to see if their colors match
+function winCheck(one, two, three, four) {
+    return (one == two && one == three && one == four && one !== 'white');
+}
 
-function makeHtmlBoard() {
-    let htmlBoard = document.getElementById('board');
-    // create the 'insertion' point for each piece at the top of the
-    // game board. Add an event listener to check which column the 
-    // piece is placed/dropped
-    let top = document.createElement("tr");
-    top.setAttribute("id", "column-top");
-    top.addEventListener("click", handleClick);
 
-    for (let x = 0; x < WIDTH; x++) {
-        let headCell = document.createElement("td");
-        headCell.setAttribute("id", x);
-        top.append(headCell);
-    }
-    htmlBoard.append(top);
-
-    //creating thje 6x7 game board and assigning coordinates
-    // to each cell (x,y). Top left corner is (0,0) and bottom
-    //right corner is (7,6). 
-    for (let y = 0; y < HEIGHT; y++) {
-        const row = document.createElement("tr");
-        for (let x = 0; x < WIDTH; x++) {
-        const cell = document.createElement("td");
-        cell.setAttribute("id", `${y}-${x}`);
-        row.append(cell);
+//horizontal check to see if there are 4 matching color rows side by side
+function horizontalCheck() {
+    for (let row = 0; row < tableRow.length; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (winCheck(tableRow[row].children[col].style.backgroundColor,
+                tableRow[row].children[col + 1].style.backgroundColor,
+                tableRow[row].children[col + 2].style.backgroundColor,
+                tableRow[row].children[col + 3].style.backgroundColor)) {
+                return true;
+            }
         }
-        htmlBoard.append(row);
     }
-}
+};
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
-function findSpotForCol(x) {
-  for (let i = HEIGHT; i > 0; i--) {
-    const filled = document.getElementById(`${i}-${x}`);
-    if (filled.childElementCount === 0) {
-      return i;
+//vertical check to see if there are 4 matching vertical color rows above one another
+function verticalCheck() {
+    for (let col = 0; col < 7; col++) {
+        for (let row = 0; row < 3; row++) {
+            if (winCheck(tableRow[row].children[col].style.backgroundColor,
+                tableRow[row + 1].children[col].style.backgroundColor,
+                tableRow[row + 2].children[col].style.backgroundColor,
+                tableRow[row + 3].children[col].style.backgroundColor)) {
+                return true;
+            }
+        }
     }
-  }
-  return null;
-}
+};
 
-/** placeInTable: update DOM to place piece into HTML table of board */
+// horizontal check - there are two horizontal checks to perform
+function diagonalCheckOne() {
+    for (let col = 0; col < 4; col++) {
+        for (let row = 0; row < 3; row++) {
+            if (winCheck(tableRow[row].children[col].style.backgroundColor,
+                tableRow[row + 1].children[col + 1].style.backgroundColor,
+                tableRow[row + 2].children[col + 2].style.backgroundColor,
+                tableRow[row + 3].children[col + 3].style.backgroundColor)) {
+                return true;
+            }
+        }
+    }
+};
 
-function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
-}
+//next diagonal check;
+function diagonalCheckTwo() {
+    for (let col = 0; col < 4; col++) {
+        for (let row = 5; row > 2; row--) {
+            if (winCheck(tableRow[row].children[col].style.backgroundColor,
+                tableRow[row - 1].children[col + 1].style.backgroundColor,
+                tableRow[row - 2].children[col + 2].style.backgroundColor,
+                tableRow[row - 3].children[col + 3].style.backgroundColor)) {
+                return true;
+            }
+        }
+    }
+};
 
-/** endGame: announce game end */
+function drawCheck() {
+    let fullSlot = [];
+    for (let i = 0; i < tableCell.length; i++) {
+        if (tableCell[i].style.backgroundColor !== 'white') {
+            fullSlot.push(tableCell[i]);
+        }
+    }
 
-function endGame(msg) {
-  // TODO: pop up alert message
-  
-}
-
-/** handleClick: handle click of column top to play piece */
-
-function handleClick(evt) {
-  // get x from ID of clicked cell
-  let x = +evt.target.id;
-
-  // get next spot in column (if none, ignore click)
-  let y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
-
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  placeInTable(y, x);
-
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
-
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-}
-
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
-
-function checkForWin() {
-  function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
-    return cells.every(
-      ([y, x]) =>
-        y >= 0 &&
-        y < HEIGHT &&
-        x >= 0 &&
-        x < WIDTH &&
-        board[y][x] === currPlayer
-    );
-  }
-
-  // TODO: read and understand this code. Add comments to help you.
-  //this is the check to see if there are 4 'blocks' in a row with matching 
-  // values in that position. it's checking if there's a match horitzontally
-  // vertically and in each diagonal
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+    if (fullSlot.length === tableCell.length) {
         return true;
-      }
     }
-  }
-}
+};
 
-makeBoard();
-makeHtmlBoard();
+//reset button which allows for the continuation of the game, it will
+// set the next turn as the person who just finished the game
+resetBtn.addEventListener('click', () => {
+    tableSlot.forEach(slot => {
+        slot.style.backgroundColor = 'white';
+    });
+
+    playerTurn.style.color = 'black';
+    return (currentPlayer == playerOne ? playerTurn.textContent == `${playerOne}'s Turn!` : playerTurn.textContent == `${playerTwo}'s turn!`);
+});
+
+
